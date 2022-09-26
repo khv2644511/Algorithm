b3cad37070009651c1a468ee2a1c62b102ac6ba7
HYEBIN KIM <khv2644511@naver.com>
2022-09-26 23:02:31 +0900

    [level 1] Title: 자릿수 더하기, Time: 3.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,17 +1,11 @@
-function solution(n)
-{
-    var numLIst = [];
-    var answer = 0;
+function solution(n) {
+    var arr = n.toString().split('');
+    console.log(arr)
     var sum = 0;
-    
-    var n = n.toString();
-    for (let i=0; i<=n.length-1; i++){
-        numLIst.push(n[i]);}
-    for (var j in numLIst) {
-    sum += parseInt(numLIst[j]);
-    }
-        
-    var answer = sum;
-    console.log(answer);
-    return answer;
+    arr.forEach(element => {
+        sum += parseInt(element);
+    });
+    return sum;
 }
+        
+
