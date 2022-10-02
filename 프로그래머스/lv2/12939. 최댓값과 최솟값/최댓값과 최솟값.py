8b046b46f48b91731640d7b5f631586ca2120b4b
HYEBIN KIM <khv2644511@naver.com>
2022-10-02 11:55:11 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -2,5 +2,7 @@ def solution(s):
     p = list(map(int, s.split()))
     max_value = max(p)
     min_value = min(p)
-    answer =(f'{min_value} {max_value}')
-    return answer
+    # answer =(f'{min_value} {max_value}')
+    
+    # return answer
+    return (str(min_value) + " " + str(max_value))
