3142ac71ae8b19ba8192bbef81544bb00d761d8c
HYEBIN KIM <khv2644511@naver.com>
2022-10-06 17:17:39 +0900

    [level 2] Title: 올바른 괄호, Time: 10.14 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,19 +1,7 @@
 def solution(s):
-    올바른_괄호 = []
-    vps = True
-    for i in s:
-        if i == '(':
-            올바른_괄호.append(i)
+    answer = True
+    
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
 
-        elif i == ')':
-            if len(올바른_괄호) > 0:
-                올바른_괄호.pop()
-            else:
-                vps = False
-
-    if len(올바른_괄호) == 0 and vps == True:
-        print('True')
-        return True
-    else:
-        print('false')
-        return False
+    return True
