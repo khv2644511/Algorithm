4617dde14a22e4cf740e5a0495047b3369495d91
HYEBIN KIM <khv2644511@naver.com>
2022-11-06 20:18:15 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,3 +1,13 @@
 function solution(my_string) {
     return my_string.split('').reverse().join('')
 }
+
+// function solution(my_string) {
+//     return my_string.split('').reverse().join('');
+// }
+// function solution(my_string) {
+//     return [...my_string].reverse().join('');
+// }
+// function solution(my_string) {
+//     return Array.from(my_string).reverse().join('');
+// }
