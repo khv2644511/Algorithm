b937688021f7b28fbccb76149d7fbd445c6cd9d9
HYEBIN KIM <khv2644511@naver.com>
2022-11-06 21:46:18 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    return Array.from(my_string)
+        .filter((t) => !["a", "e", "i", "o", "u"].includes(t))
+        .join("");
+}
+
+// function solution(my_string) {
+//     return my_string.replace(/[aeiou]/g, "");
+// }
