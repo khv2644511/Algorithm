58054174ae121a87411d9a1dda51e1785d428c90
HYEBIN KIM <khv2644511@naver.com>
2022-11-29 09:34:47 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (1) - 120851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.08 ms
+메모리: 33.6 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,14 @@
 
+// function solution(my_string) {
+//     return my_string
+//         .match(/[0-9]/g)
+//         .reduce((a, b) => parseInt(a) + parseInt(b));
+// }
+
 function solution(my_string) {
-    return my_string
-        .match(/[0-9]/g)
-        .reduce((a, b) => parseInt(a) + parseInt(b));
+    return Array
+            .from(my_string)
+            .map(v => +v)
+            .filter(v => !Number.isNaN(v))
+            .reduce((a,c) => a+c,0)
 }
