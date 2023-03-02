85ca873b22f8e23950373752a701ffcc1f8f161f
HYEBIN KIM <khv2644511@naver.com>
2023-03-02 14:42:29 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,18 @@
 function solution(nums) {
-  let select = [];
+  //   let select = [];
   const set = new Set(nums);
   const answer = [...set];
-  for (let i = 0; i < nums.length / 2; i++) {
-    if (answer[i]) {
-      select.push(answer[i]);
-      //   console.log(select);
-    }
-  }
-  //   console.log(select.length);
-  return select.length;
+  let noDuplicateCount = answer.length;
+  let pocketmonCount = nums.length;
+  return noDuplicateCount > pocketmonCount / 2
+    ? pocketmonCount / 2
+    : noDuplicateCount;
+  //   for (let i = 0; i < nums.length / 2; i++) {
+  //     if (answer[i]) {
+  //       select.push(answer[i]);
+  //       //   console.log(select);
+  //     }
+  //   }
+  //   //   console.log(select.length);
+  //   return select.length;
 }
