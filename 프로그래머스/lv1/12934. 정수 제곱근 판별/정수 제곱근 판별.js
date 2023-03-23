f22465239cce3e9e906b7d90d53bbc4b17f5f475
HYEBIN KIM <khv2644511@naver.com>
2023-03-23 22:25:48 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 3.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,19 +1,7 @@
 function solution(n) {
-  let answer = 0;
-  if (n === 1) {
-    return (answer = 4);
-  }
-  if (n === 2) {
-    return (answer = -1);
-  }
-  if (n === 3) {
-    return (answer = -1);
-  }
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) {
-      answer = (i + 1) ** 2;
-    } else answer = -1;
-  }
-
-  return answer;
+  var result = 0;
+  var n = Math.sqrt(n);
+  console.log(n);
+  result = Number.isInteger(n) ? Math.pow(n + 1, 2) : -1;
+  return result;
 }
