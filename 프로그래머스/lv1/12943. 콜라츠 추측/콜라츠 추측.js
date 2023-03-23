461319b49c333458738f6bb462588875321c8889
HYEBIN KIM <khv2644511@naver.com>
2023-03-24 01:38:18 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,21 +1,8 @@
 function solution(num) {
-  let answer = 0;
-  if (num === 1) {
-    return (answer = 0);
+    var answer = 0;
+    while(num !=1 && answer !=500){
+        num%2==0 ? num = num/2 : num = num*3 +1;
+    answer++;
   }
-  while (num > 1) {
-    if (num % 2 === 0) {
-      num = num / 2;
-      answer += 1;
-    } else if (num % 2 === 1) {
-      num = num * 3 + 1;
-      answer += 1;
-    }
-    if (answer === 500) {
-      return (answer = -1);
-    }
-    if (num === 1) {
-      return answer;
-    }
-  }
+    return num == 1 ? answer : -1;
 }
