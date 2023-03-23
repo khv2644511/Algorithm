a92176df730490ee980e39d15b6b04bc540520a8
HYEBIN KIM <khv2644511@naver.com>
2023-03-24 01:53:10 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,9 +1,4 @@
-function solution(phone_number) {
-  var answer = "";
-  let phoneArr = phone_number.split("");
-  let lastNum = phoneArr.splice(phone_number.length - 4);
-  let startNum = phoneArr.splice(0, phone_number.length - 4);
-  answer = Array(startNum.length).fill("*").concat(lastNum);
-  answer = answer.join("");
-  return answer;
+function solution(s) {
+  var result = "*".repeat(s.length - 4) + s.slice(-4);
+  return result;
 }
