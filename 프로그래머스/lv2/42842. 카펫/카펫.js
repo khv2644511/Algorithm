980619c8512703db910a04a0912c914d06a7ed2c
HYEBIN KIM <khv2644511@naver.com>
2023-03-26 11:49:24 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,9 +1,9 @@
 function solution(brown, yellow) {
-  var answer = [];
-  // yellow로 만들 수 있는 가로가 긴 사각형
-  let x = [];
+  // 최소 높이는 3 이상이 됨
   for (let y = 3; y <= brown; y++) {
+    // 전체 높이가 될 수 있는 y를 구함
     if ((yellow + brown) % y === 0) {
+      // 전체 높이를 통해 가로를 구함
       let x = (yellow + brown) / y;
       if ((x - 2) * (y - 2) === yellow) {
         return [x, y];
