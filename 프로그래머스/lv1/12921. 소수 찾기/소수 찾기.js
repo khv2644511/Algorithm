a0e7a5a335cf24f4bf0084ae2eb968bc44d4e03e
HYEBIN KIM <khv2644511@naver.com>
2023-03-29 11:25:39 +0900

    [level 1] Title: 소수 찾기, Time: 93.13 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,18 +1,19 @@
 function solution(n) {
-  let answer = [];
-  
-  function isPrime (n) {
-    for(let x of answer) {
-      if(x>Math.sqrt(n))return true
-      if(Number.isInteger(n/x)) return false
+    var arr =[];
+    for(var i=0; i<=n; i++){
+        arr.push(true);
     }
-    return true
-  }
-  
-  for(let i=2; i<=n; i++) {
-		if(!i%2) continue
-    if(isPrime(i)) answer.push(i)
-  }
-  
-  return answer.length;
+    
+    arr[0] = false;
+    arr[1] = false;
+    
+    for(var i=2; i<=n; i++){
+        if (arr[i]) {
+            for(var k=2; k<=n/i; k++){
+                arr[i*k] = false;
+            }
+        }
+    }
+    
+    return arr.filter(bool => bool === true).length;
 }
