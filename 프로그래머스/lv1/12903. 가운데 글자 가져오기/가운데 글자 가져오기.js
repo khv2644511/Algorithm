94f0a4952ccbffe9144663c32dbecf0ef22b1cc9
HYEBIN KIM <khv2644511@naver.com>
2023-03-30 15:51:27 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  var answer = "";
+  if (s.length % 2 === 0) {
+    answer = s.slice(s.length / 2 - 1, s.length / 2 + 1);
+  }
+  if (s.length % 2 !== 0) {
+    answer = s.slice(s.length / 2, s.length / 2 + 1);
+  }
+  return answer;
+}
