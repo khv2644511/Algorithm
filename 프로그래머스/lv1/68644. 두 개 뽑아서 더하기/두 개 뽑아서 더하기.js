f88230dc47d8847ed9cdbfaae5df4a9a0a91122a
HYEBIN KIM <khv2644511@naver.com>
2023-04-11 12:44:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.36 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  var answer = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      if (answer.indexOf(sum) === -1) {
+        answer.push(sum);
+      }
+    }
+  }
+  return answer.sort((a, b) => a - b);
+}
