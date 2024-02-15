b47e43b3b1b7553a020c2c4a3588bb4f2a18b921
HYEBIN KIM <khv2644511@naver.com>
2024-02-15 12:56:22 +0900

    [level 0] Title: a와 b 출력하기, Time: 46.28 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -0,0 +1,24 @@
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//     input = line.split(' ');
+// }).on('close', function () {
+//     console.log(`a = ${Number(input[0])}\nb = ${Number(input[1])}`);
+// });
+
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line', function (line) {
+    const [a, b] = line.split(' ')
+    console.log('a =', a)
+    console.log('b =', b)
+})
