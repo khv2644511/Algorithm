7f76b669c3f1a275f07ec853e71c86fdb6e247ca
HYEBIN KIM <khv2644511@naver.com>
2024-03-12 12:58:32 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 828 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.js
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.js
@@ -1,27 +1,12 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const size = input.shift();
-
-for (let i = 0; i < size; i++) {
-    let result = [];
-
-    let inputArr = input[i].split(' ');
-
-    for (let j = 0; j < inputArr.length; j++) {
-        let stack = [];
-        let reverse = [];
-
-        let str = inputArr[j];
-        for (let k = 0; k < str.length; k++) {
-            stack.push(str[k]);
-        }
-
-        while (stack.length > 0) {
-            reverse.push(stack.pop());
-        }
-
-        result.push(reverse.join(''));
+let len = parseInt(input[0]);
+for (let i = 1; i <= len; i++) {
+    let words = input[i].split(' ');
+    let answer = '';
+    for (let word of words) {
+        answer += `${word.split('').reverse().join('')} `;
     }
-    console.log(result.join(' '));
+    console.log(answer);
 }
