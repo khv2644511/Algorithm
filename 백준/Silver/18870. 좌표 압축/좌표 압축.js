96579e1f2415814f425b0867b64c45b439e4204b
HYEBIN KIM <khv2644511@naver.com>
2024-03-15 01:09:14 +0900

    [Silver II] Title: 좌표 압축, Time: 2052 ms, Memory: 345144 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -1,21 +1,23 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const size = parseInt(input[0]);
-const X = input[1].split(' ').map(Number);
-
-const orderedX = input[1]
+const size = parseInt(input[0]); // N : 주어진 좌표 개수
+const X = input[1].split(' ').map(Number); // X1, X2, ... ,Xn
+const orderedX = input[1] // uniqueX를 만들기1 - 오름차순 정렬
     .split(' ')
     .map(Number)
     .sort((a, b) => a - b);
-const set = new Set(orderedX);
+const set = new Set(orderedX); // uniqueX 만들기2 - 중복 제거하기
 const uniqueX = [...set];
+let result = []; // 정답 출력을 위한 변수
 
-let result = [];
+// console.log('X', X);
+// console.log('orderedX', orderedX);
+// console.log('uniqueX', uniqueX);
 
-for (let i = 0; i < size; i++) {
-    let arr = uniqueX;
-    let key = X[i];
+for (let i = 0; i < size; i++) { // N개 만큼 순회
+    let arr = uniqueX; // uniqueX를 탐색
+    let key = X[i]; // X는 탐색키
     let low = 0;
     let high = uniqueX.length;
     let idx = binary_search(arr, key, low, high);
@@ -25,7 +27,6 @@ for (let i = 0; i < size; i++) {
 function binary_search(arr, key, low, high) {
     if (low <= high) {
         let middle = ~~((low + high) / 2);
-
         if (arr[middle] == key) {
             return middle;
         } else if (arr[middle] > key) {
