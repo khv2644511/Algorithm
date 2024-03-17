d48dcd8ca9a89daf699532b03b6d8bf816f08a77
HYEBIN KIM <khv2644511@naver.com>
2024-03-18 02:38:40 +0900

    [level 3] Title: 입국심사, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?itm_content=course14743) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.30 ms
+메모리: 33.5 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 17일 17:26:03
+2024년 03월 18일 02:38:38
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,24 +1,24 @@
 function solution(n, times) {
-    times.sort((a,b) => a-b); 
-    let left = 1;
-    let right = n * times[times.length -1];
-    let answer = right; // 최대값.
-    while(left <= right){
-        let mid = Math.floor((left + right) / 2);
-        let count = 0
-        times.forEach(value => {
-            count += Math.floor(mid / value); // 한 사람당 몇명 할 수 있는지
-            if(count >= n) {
-                answer = Math.min(mid, answer); // 최솟값
-                return;
-            };
-        });
-        if (count >= n) { 
-            right = mid - 1;
+    let low = 1; // MaxH가 가질 수 있는 최솟값
+    let high = Math.max(...times) * n; // time 가질 수 있는 최댓값
+    times.sort((a, b) => a - b);
+
+    let answer = 0;
+    while (low <= high) {
+        let middle = ~~((low + high) / 2);
+        let people = 0;
+        for (let time of times) {
+            people += ~~(middle / time);
+            if (people >= n) break;
         }
-        else {
-            left = mid + 1;
+
+        if (people >= n) {
+            high = middle - 1;
+            answer = middle;
+        } else {
+            low = middle + 1;
         }
     }
     return answer;
 }
+// console.log(solution(6, [7, 10]));
