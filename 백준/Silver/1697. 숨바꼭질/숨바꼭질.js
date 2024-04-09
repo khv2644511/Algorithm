d1d9ef4ff2d56b5084f3344bec3f49d08fcc0ed0
HYEBIN KIM <khv2644511@naver.com>
2024-04-09 17:15:22 +0900

    [Silver I] Title: 숨바꼭질, Time: 3564 ms, Memory: 20808 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,30 +1,31 @@
-const [N, K] = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split(" ")
-  .map(Number);
+const fs = require('fs');
+// const input = fs.readFileSync('./input.txt').toString().trim();
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+let [N, K] = input.split(' ').map(Number);
 
-const queue = [[N, 0]];
 const visited = Array(100001).fill(false);
 
-while (queue.length) {
-  const [cur, depth] = queue.shift();
+function bfs(v) {
+    let queue = [];
+    queue.push([v, 0]); // 초기 정점과 깊이(0)을 함께 큐에 추가
 
-  if (visited[cur]) {
-    continue;
-  }
+    while (queue.length !== 0) {
+        const [cur, depth] = queue.shift();
 
-  visited[cur] = true;
+        if (cur === K) {
+            console.log(depth);
+            return; // 목표 정점에 도달하면 함수 종료
+        }
 
-  if (cur === K) {
-    console.log(depth);
-    break;
-  }
+        visited[cur] = true; // 현재 정점 방문 처리
 
-  for (let next of [cur + 1, cur - 1, cur * 2]) {
-    if (!visited[next] && next >= 0 && next <= 100000) {
-      queue.push([next, depth + 1]);
+        for (let next of [cur - 1, cur + 1, cur * 2]) { // 가능한 모든 이동 시도
+            if (next >= 0 && next <= 100000 && !visited[next]) {
+                queue.push([next, depth + 1]); // 방문하지 않은 정점만 큐에 추가
+                visited[next] = true; // 바로 방문 처리하여 중복 방지
+            }
+        }
     }
-  }
 }
+
+bfs(N);
