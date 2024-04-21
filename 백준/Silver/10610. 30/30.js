8c8ff7f1da4690568425b2c5036859ee10096c3a
HYEBIN KIM <khv2644511@naver.com>
2024-04-21 17:49:07 +0900

    [Silver IV] Title: 30, Time: 208 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.js
+++ 백준/Silver/10610. 30/30.js
@@ -1,10 +1,28 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
-const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+let inputArr = input[0].split('').map(Number);
+let answer = -1;
 
-const numArr = input.split("").map(Number);
+// 내림차순 정렬
+inputArr.sort((a, b) => b - a);
 
-numArr.sort((a, b) => b - a);
+// // 0이 없는 경우는 return -1
+// if (!inputArr.includes(0)) {
+//     console.log(answer);
+//     return;
+// }
+// // 맨처음이 0인 경우 => 0만 있는 경우
+// if (inputArr[0] === 0) {
+//     console.log(answer);
+//     return;
+// }
+const sum = inputArr.reduce((acc, cur) => acc + cur, 0);
+if (sum % 3 == 0 && inputArr.includes(0)) {
+    answer = inputArr.join('');
+} else {
+    answer;
+}
 
-const sum = numArr.reduce((acc, cur) => acc + cur);
-
-console.log(sum % 3 === 0 && numArr.includes(0) ? numArr.join("") : -1);
+console.log(answer);
