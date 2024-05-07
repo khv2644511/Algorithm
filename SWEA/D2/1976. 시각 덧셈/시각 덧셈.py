9d2c3900d860df706d70452e7fa6fd8537c18629
HYEBIN KIM <khv2644511@naver.com>
2024-05-07 10:19:51 +0900

    [D2] Title: 시각 덧셈, Time: 97 ms, Memory: 44,724 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+for i in range(1, T+1):
+    a,b,c,d = map(int, input().split())
+    # print(a,b,c,d)
+
+    hour = a+c
+    minutes = b+d
+    addhour = 0
+
+    if minutes>=60:
+        addhour = minutes//60
+        minutes = minutes%60
+    
+    if addhour> 0:
+        hour += addhour
+
+    if hour >= 12:
+        hour = hour%12
+        
+    if hour == 12 or hour==0:
+        hour = 12
+
+    print(f"#{i} {hour} {minutes}")
