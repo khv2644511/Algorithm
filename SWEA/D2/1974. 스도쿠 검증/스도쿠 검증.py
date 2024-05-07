ea080c9e48552c105d999592707c2de8f9eb4ff4
HYEBIN KIM <khv2644511@naver.com>
2024-05-07 15:22:15 +0900

    [D2] Title: 스도쿠 검증, Time: 108 ms, Memory: 45,252 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,41 +1,42 @@
 T = int(input())
+
+
 for t in range(1, T+1):
     answer = 1
     arr = []
+
     for j in range(1,10):
         lst = list(map(int, input().split()))
         arr.append(lst)
-        # 행 확인
-        for a in range(1,10):
-            if lst.count(a)==0  or lst.count(a) >1:
-                answer = 0
-                break
+
+
+    # 행 확인
+    for row in arr:
+        if len(set(row)) !=9:
+            answer = 0
+            break
 
     # 열 확인
-    for a in range(0,9):
-        checkColList = []
-        for b in range(0,9):
-            checkColList.append(arr[b][a])
+    for col in range(9):
+        column = [arr[row][col] for row in range(9)]
+        if len(set(column)) !=9:
+            answer = 0
+            break
 
-        for k in range(1,10):
-            if checkColList.count(k)==0  or checkColList.count(k) >1:
-                answer = 0
-                break
-
-    # 각 3x3 부분 배열을 추출하고 출력
+    # 각 3x3 확인
     for i in range(0, 9, 3):
         for j in range(0, 9, 3):
             check3x3List = []   
             # 3x3 부분 배열 생성
+            # square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
             for x in range(i, i+3):
                 for y in range(j, j+3):
-                    # print(f"({x},{y}) {arr[x][y]}")
-                    check3x3List.append(arr[x][y])
 
-            # print(check3x3List)
-            for k in range(1,10):
-                if check3x3List.count(k)==0  or check3x3List.count(k) >1:
-                    answer = 0
-                    break
+                    check3x3List.append(arr[x][y])
+                    # print(x, y)
+            if len(set(check3x3List)) !=9:
+                answer = 0
+                break
+
 
     print(f'#{t} {answer}')
