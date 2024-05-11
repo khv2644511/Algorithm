a44f546e3c4d7da1073efc28eef084ba0d157cb9
HYEBIN KIM <khv2644511@naver.com>
2024-05-11 17:15:25 +0900

    [D2] Title: 가랏! RC카!, Time: 101 ms, Memory: 44,992 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -1,25 +1,17 @@
 T = int(input())
-for t in range(1,T+1):
-    N = int(input())
-
-    speed = 0
-    distance= 0
-    for i in range(N):
-        n = input()
-        # print(n)
-        if len(n)!=1:
-            a, b = map(int, n.split())
-            if a ==1: # 가속
-                speed += b
-            else: # 감속
-                if speed < b:
-                    speed= 0
-                else:
-                    speed -=b
-
-            distance += speed
-
-        else:
-            distance += speed
-
-    print(f'#{t} {distance}')
+ 
+for case in range(1, T+1):
+    count = int(input())
+    distance = 0
+    acc = 0
+     
+    for _ in range(count):
+        command = list(map(int, input().split()))
+        if command[0] == 1:
+            acc += command[1]
+        elif command[0] == 2:
+            acc = max(0, acc - command[1])
+         
+        distance += acc
+     
+    print(f"#{case} {distance}")
