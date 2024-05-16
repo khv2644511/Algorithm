20927b2a470c6a09914fabc371319c566624c727
HYEBIN KIM <khv2644511@naver.com>
2024-05-16 17:39:29 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 172 ms, Memory: 50,292 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -1,17 +1,18 @@
 for test_case in range(10):
     N = int(input())
 
-    arr = [list(input().split()) for _ in range(N)]
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
     col_arr = [[arr[j][i] for j in range(N)] for i in range(N)]
     count = 0
 
-    for k in range(N):
-        while '0' in col_arr[k]:
-            col_arr[k].remove('0')
-        line = ''.join(col_arr[k])
-
-        for idx in range(len(line)-1):
-            if line[idx: idx+2] == '12':
-                count +=1
+    for i in range(N):
+        state = 0
+        for j in range(N):
+            if col_arr[i][j] == 1:
+                state = 1
+            if col_arr[i][j] == 2 and state == 1:
+                count += 1
+                state = 0
 
     print(f'#{test_case+1} {count}')
