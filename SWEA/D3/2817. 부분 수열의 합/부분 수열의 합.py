00610856a0df8a2a32ed2220c8071bf3a2a770a4
HYEBIN KIM <khv2644511@naver.com>
2024-05-18 01:09:37 +0900

    [D3] Title: 부분 수열의 합, Time: 5,688 ms, Memory: 49,460 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -8,13 +8,17 @@ for tc in range(1, T + 1):
 
     for i in range(1<< N):
         subset_sum = 0
+        nums = []
         for j in range(N):
             if i & (1<<j):
+                nums.append(j)
                 subset_sum += A[j]
+        # print(nums)
 
         if subset_sum != K:
             continue
 
         if subset_sum == K:
             count += 1
+
     print(f"#{tc} {count}")
