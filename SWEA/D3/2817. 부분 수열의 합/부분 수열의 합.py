9f3d6fdf3a3f70c3584d6c680472abd966957dcf
HYEBIN KIM <khv2644511@naver.com>
2024-05-17 17:37:19 +0900

    [D3] Title: 부분 수열의 합, Time: 3,873 ms, Memory: 48,672 KB -BaekjoonHub

---
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    count = 0
+
+    for i in range(1<< N):
+        subset_sum = 0
+        for j in range(N):
+            if i & (1<<j):
+                subset_sum += A[j]
+
+        if subset_sum != K:
+            continue
+
+        if subset_sum == K:
+            count += 1
+    print(f"#{tc} {count}")
