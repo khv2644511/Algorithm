61c876551292fb320728631647040f7d004d0d98
HYEBIN KIM <khv2644511@naver.com>
2024-05-18 21:32:18 +0900

    [D2] Title: 쉬운 거스름돈, Time: 114 ms, Memory: 44,712 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
@@ -1,17 +1,13 @@
 T = int(input())
-for test_case in range(1, T+1):
-    N = int(input())
+S = [50000,10000,5000,1000,500,100,50,10]
 
-    price = [50000,10000,5000,1000,500,100,50,10]
-    count = []
+for tc in range(1, T+1):
+    price = int(input())
+    answer = ''
+    for i in S:
+        count = 0
+        answer += str(price // i) + ' '
+        price = price % i
 
-    for i in range(len(price)):
-        if N >= price[i]:
-            count.append(N//price[i])
-            N = N % price[i]
-        else:
-            count.append(0)
-        
-
-    print(f'#{test_case}')
-    print(' '.join(map(str, count)))
+    print(f'#{tc}')
+    print(f'{answer}')
