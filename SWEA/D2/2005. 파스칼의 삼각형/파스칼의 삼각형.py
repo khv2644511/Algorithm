3a9d268e58460b3adc4a67b0fadfc3684618258d
HYEBIN KIM <khv2644511@naver.com>
2024-05-18 15:39:18 +0900

    [D2] Title: 파스칼의 삼각형, Time: 113 ms, Memory: 45,268 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,22 +1,21 @@
 T = int(input())
 
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [[0 for j in range(N)] for i in range(N)]
+    # print(arr)
+    for k in range(N):
+        arr[k][0] = 1
 
-for i in range(1, T+1):
-    n = int(input())
+    for i in range(1,N):
+        for j in range(1, N):
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
 
-    arr = [[0 for j in range(n)] for i in range(n)]
-    print(f'#{i}')
-    if n==1:
-        print(1)
-        continue
+    print(f'#{tc}')
 
-    for j in range(0, n):
-        for k in range(0, j+1):
-            # print(i+k, end=' ')
-            # lst[j].append(k)
-            # print(j, end=' ')
-            arr[j][k] = 1
-            if arr[j-1][k-1]+arr[j-1][k] > 1:
-                arr[j][k]= arr[j-1][k-1]+arr[j-1][k]
-            print(arr[j][k]  , end=' ')
-        print('')
+    for i in range(N):
+        answer = ' '.join(map(str, arr[i]))
+        for j in answer:
+            if j != '0':
+                print(j, end='')
+        print()
