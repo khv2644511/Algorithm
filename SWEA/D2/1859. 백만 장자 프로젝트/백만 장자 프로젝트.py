3dd5a4c1b2c89399a17c519e183c55dea424420f
HYEBIN KIM <khv2644511@naver.com>
2024-05-18 14:43:26 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,160 ms, Memory: 168,240 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -4,19 +4,14 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     price_list = list(map(int, input().split()))
-
+    price_list.reverse()
     answer = 0
-    start = 0
 
-    while start < N:
-        i_max = start
-        for i in range(start+1, N):
-            if price_list[i_max] < price_list[i]:
-                i_max = i
-
-        for i in range(start, i_max):
-            answer += price_list[i_max] - price_list[i]
-
-        start = i_max + 1
+    max = price_list[0]
+    for price in price_list:
+        if price > max:
+            max = price
+        else:
+            answer += max - price
 
     print(f'#{tc} {answer}')
