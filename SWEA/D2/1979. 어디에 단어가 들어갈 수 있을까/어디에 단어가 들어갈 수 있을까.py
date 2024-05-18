fed3271ab30adc2b91d0e290a434cdfdc10948f5
HYEBIN KIM <khv2644511@naver.com>
2024-05-18 18:54:45 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 112 ms, Memory: 45,200 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,36 +1,23 @@
-n = int(input())
+T = int(input())
 
-for i in range(1, n+1):
-    n, k = map(int, input().split())
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    line = [[arr[j][i] for j in range(N)] for i in range(N)]
+    # print(line)
+    result = 0
+    searchStr = '1' * K
 
-    arr = []
-    for j in range(n):
-        lst = list(map(int, input().split()))
-        arr.append(lst)
-
-
-
-    result= 0
-    searchStr = '1' *k
     #  가로 개수세기
-    for a in range(n):
-        string = ''.join(map(str, arr[a]))
-
+    for i in range(N):
+        string = ''.join(map(str, arr[i]))
         searchList = string.split('0')
-        # print(searchList)
-        
         result += searchList.count(searchStr)
 
-
     # 세로 개수세기
-    for a in range(n):
-        colArr = []
-        colStr = ''
-        for b in range(n):
-            colStr += str(arr[b][a])
-        searchColList = colStr.split('0')
+    for i in range(N):
+        string = ''.join(map(str, line[i]))
+        searchList = string.split('0')
+        result += searchList.count(searchStr)
 
-        # print(searchColList)
-        result += searchColList.count(searchStr)
-
-    print(f'#{i} {result}')
+    print(f'#{tc} {result}')
