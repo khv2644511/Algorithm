9ad01ceeca29cb7be7c87e689c579de8a2a2bfd2
HYEBIN KIM <khv2644511@naver.com>
2024-05-18 21:17:58 +0900

    [D2] Title: 스도쿠 검증, Time: 109 ms, Memory: 44,724 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,36 +1,29 @@
 T = int(input())
 
-for t in range(1, T+1):
+for tc in range(1, T+1):
+    arr = [list(map(int, input().split())) for i in range(9)]
+    line = [[arr[j][i] for j in range(9)] for i in range(9)]
+    # print(line)
     answer = 1
-    arr = []
 
-    for j in range(1,10):
-        lst = list(map(int, input().split()))
-        arr.append(lst)
-
-
-    # 행 확인
-    for row in arr:
-        if len(set(row)) !=9:
+    for i in range(9):
+        set_list = set(arr[i])
+        if len(set_list) != 9:
             answer = 0
-            break
 
-    # 열 확인
-    for col in range(9):
-        column = [arr[row][col] for row in range(9)]
-        if len(set(column)) !=9:
+    for i in range(9):
+        set_list = set(line[i])
+        if len(set_list) != 9:
             answer = 0
-            break
 
-    # 각 3x3 확인
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            check3x3List = []   
-            # 3x3 부분 배열 생성
-            square = [arr[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-            if len(set(square)) !=9:
+    for a in range(0, 9, 3):
+        for b in range(0, 9, 3):
+            lst = []
+            for c in range(3):
+                for d in range(3):
+                    lst.append(arr[a+c][b+d])
+            # print(tc, len(set(lst)))
+            if len(set(lst)) != 9:
                 answer = 0
-                break
 
-
-    print(f'#{t} {answer}')
+    print(f'#{tc} {answer}')
