4d1903b16d7003f314d5bb772cb1cbf439aed6ab
HYEBIN KIM <khv2644511@naver.com>
2024-05-18 17:39:17 +0900

    [D2] Title: 조교의 성적 매기기, Time: 184 ms, Memory: 45,444 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,18 +1,23 @@
-grades = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+T = int(input())
 
-t = int(input())
-for tc in range(t):
+# 점수
+score = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+','C0', 'C-', 'D0']
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
 
-  n,k = map(int,input().split())
-  arr = []
-  
-  for i in range(n):
-    mid, final, assign = map(int,input().split())
-    score = mid*0.35 + final * 0.45 + assign * 0.2
-    arr.append(score)
+    score_list = {}
+    for i in range(N):
+        중간, 기말, 과제 = map(int, input().split())
+        총점 = 중간*0.35 + 기말*0.45 + 과제*0.2
+        score_list[i] = 총점
 
-  target = arr[k-1]
-  arr.sort(reverse=True)
-  rate = n//10
-  ratio_score = arr.index(target) // rate
-  print('#{} {}'.format(tc+1, grades[ratio_score]))
+    sorted_lst = sorted(score_list.values(), reverse=True)
+
+
+    # score_list = [list(map(int, input().split())) for _ in range(N)]
+    # print(score_list)
+    # print(score_list[K-1]) # 찾는 학생의 점수
+    # print(sorted_lst.index(score_list[K-1])) # 찾는 점수가 몇등인지에 대한 인덱스, 인덱스 0 ~ N-1
+    searchIdx = sorted_lst.index(score_list[K-1]) # 정렬된 리스트에서 찾으려는 점수의 인덱스를 찾는다
+
+    print(f'#{tc} {score[searchIdx // (N//10)]}')
