4c1223ad550243c04d66c9085e35c5a6e56bb14b
HYEBIN KIM <khv2644511@naver.com>
2024-07-21 00:28:55 +0900

    [level 1] Title: 추억 점수, Time: 1.87 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/README.md
+++ 프로그래머스/1/176963. 추억 점수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 추억 점수 - 176963 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 3.24 ms
+메모리: 36 MB, 시간: 1.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 19:36:49
+2024년 07월 21일 00:28:53
 
 ### 문제 설명
 
--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,26 +1,29 @@
 function solution(name, yearning, photo) {
     var answer = [];
-    let result = [];
     
-    let map = new Map();
+    let hashMap = new Map();
     
-    for (let i=0; i< name.length; i++) {
-        map.set(name[i], yearning[i])
+    for (let i = 0; i < name.length; i++) {
+        for (let j = 0; j < yearning.length; j++) {
+              if(i===j) {
+                hashMap.set(name[i], yearning[j])
+              }
+        }
     }
     
-    for (let arr of photo) {
-        let sum = 0;
-        // console.log(arr)
-        for (let i=0; i< arr.length; i++) {
-            if(map.has(arr[i])) {
-                let yearn = map.get(arr[i])
-                sum += yearn
+    // for (let [key, value] of hashMap) {
+    //     console.log(key + ' = ' + value)
+    // }    
+    photo.map((arr) => {
+        let count = 0
+
+        for (let i = 0; i < arr.length; i++) {
+            if (hashMap.has(arr[i])) {
+                count += hashMap.get(arr[i])
             }
         }
-        result.push(sum)
-        console.log(result)
-    }
-    
-    // console.log(map)
-    return result;
+        answer.push(count)
+        // console.log(count)
+    })
+    return answer;
 }
